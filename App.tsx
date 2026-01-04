93c4559931cdff1578ec4b3b961bfe692f470ccd
[dyad] <mouragabriel2011@gmail.com>
2026-01-04 16:49:35 -0300

    [dyad] Corrigindo erro de importação - wrote 1 file(s)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
+import HomePage from './src/pages/HomePage';
+import LoginPage from './src/pages/LoginPage';
 
 const App: React.FC = () => {
   return (
