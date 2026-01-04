db04947e040e3efb2744d947b9365e981427d3bd
[dyad] <mouragabriel2011@gmail.com>
2026-01-04 15:38:03 -0300

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -263,7 +263,10 @@ const App: React.FC = () => {
               <strong>Andreza Lima dos Santos</strong>, de 27 anos, empregada doméstica e mãe de dois filhos, iniciou o processo para tirar a CNH no dia 10/12/2025, um dia após a nova resolução entrar em vigor. 
             </p>
             <p>
-              Em apenas 11 dias, no dia 21/12/2025, ela concluiu todas as etapas e realizou a prova prática. O Ministro dos Transportes entregou pessoalmente a habilitação, tornando Andreza a primeira brasileira a obter a CNH pelo novo modelo gratuito.
+              Em apenas 11 dias, no dia 21/12/2025, ela concluiu todas as etapas e realizou a prova prática. O Ministro dos Transportes Renan Filho, entregou pessoalmente a habilitação, tornando Andreza a primeira brasileira a obter a CNH pelo novo modelo gratuito.
+            </p>
+             <p>
+              Com a Resolução nº 985/2025 do Contan, mais de 1 milhão de brasileiros já se inscreveram no programa. e as vagas para 2026 estão se esgotando.
             </p>
           </div>
 
